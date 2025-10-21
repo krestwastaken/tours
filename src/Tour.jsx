04b6b9360c94510f8a19de1c01b37a65763c7ebf
krestwastaken <martynskelvin@gmail.com>
2025-10-21 23:51:54 +0100

    readmore

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  console.log(info.substring(0, 10));
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            className="info-btn"
+            type="button"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
         <button
           className="btn btn-block delete-btn"
           type="button"
